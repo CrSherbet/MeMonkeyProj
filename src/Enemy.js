296d2ca1604661273881795d6712884bf17c71ba
Warisara Inprom <Warisara Inprom>
2016-03-10 20:13:18 +0700

    Enemy can move

--- src/Enemy.js
+++ src/Enemy.js
@@ -2,5 +2,23 @@ var Enemy = cc.Sprite.extend({
     ctor: function() {
         this._super();
         this.initWithFile( 'res/images/monkey.png' );
+        this.direction = Enemy.DIRECTION.LEFT ;
+    },
+    update: function( dt ){
+        this.changeDirection();
+    },
+    
+    changeDirection: function(){
+        var pos = this.getPosition();
+        if ( pos.x > screenWidth -50 )
+            this.direction = Enemy.DIRECTION.LEFT ;
+        else if ( pos.x < 50 )
+            this.direction = Enemy.DIRECTION.RIGHT ;
+        this.setPosition( new cc.Point( pos.x + this.direction , pos.y ));
     }
 });
+
+Enemy.DIRECTION = {
+    LEFT : -5 ,
+    RIGHT :  5
+};
